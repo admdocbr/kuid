c08ae0dedcec692a63f17755c2d558c1d39cf2bb
Björn Boschman <bjoern.boschman@innovid.com>
2024-07-27 09:06:08 -0300

    seem unittest wants to be a class

--- tests/test_kuid.py
+++ tests/test_kuid.py
@@ -1,7 +1,13 @@
 import uuid
+import unittest
 from kuid import encode, decode
 
 
-def test_encode_decode():
-    u = uuid.uuid4()
-    assert decode(encode(u)) == u
+class TestKuid(unittest.TestCase):
+    def test_encode_decode(self):
+        u = uuid.uuid4()
+        assert decode(encode(u)) == u
+
+
+if __name__ == "__main__":
+    unittest.main()
