3498ea0c45017145f2088c382669d726b8ca3fed
Björn Boschman <bjoern.boschman@innovid.com>
2024-07-27 09:46:43 -0300

    Added uuid wrappers

--- kuid/__init__.py
+++ kuid/__init__.py
@@ -8,3 +8,19 @@ def encode(u: uuid.UUID) -> str:
 
 def decode(s: str) -> str:
     return uuid.UUID(int=base62.decode(s))
+
+
+def kuid1() -> str:
+    return encode(uuid.uuid1())
+
+
+def kuid3(namespace: uuid.UUID, name: str | bytes) -> str:
+    return encode(uuid.uuid3(namespace, name))
+
+
+def kuid4() -> str:
+    return encode(uuid.uuid4())
+
+
+def kuid5(namespace: uuid.UUID, name: str | bytes) -> str:
+    return encode(uuid.uuid5(namespace, name))
--- tests/test_kuid.py
+++ tests/test_kuid.py
@@ -1,12 +1,24 @@
 import uuid
 import unittest
-from kuid import encode, decode
+from kuid import encode, decode, kuid1, kuid3, kuid4, kuid5
 
 
 class TestKuid(unittest.TestCase):
     def test_encode_decode(self):
         u = uuid.uuid4()
         assert decode(encode(u)) == u
+
+    def test_kuid1(self):
+        assert isinstance(decode(kuid1()), uuid.UUID)
+
+    def test_kuid3(self):
+        assert isinstance(decode(kuid3(namespace=uuid.uuid4(), name="foo")), uuid.UUID)
+
+    def test_kuid4(self):
+        assert isinstance(decode(kuid4()), uuid.UUID)
+
+    def test_kuid5(self):
+        assert isinstance(decode(kuid5(namespace=uuid.uuid4(), name="foo")), uuid.UUID)
 
 
 if __name__ == "__main__":
